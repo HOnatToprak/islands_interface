2b42aa5877982be06b99ca32f9283b1aedd82a3a
Onat <hasan.onat.toprak@gmail.com>
2019-04-25 11:27:12 +0300

    Initial Commit

---
+++ lib/islands_interface_web/channels/game_channel.ex
@@ -0,0 +1,27 @@
+defmodule IslandsInterfaceWeb.GameChannel do
+  use IslandsInterfaceWeb, :channel
+
+  alias IslandsEngine.{Game, Supervisor}
+
+  def channel do
+    quote do
+      use Phoenix.Channel
+      import IslandsInterfaceWeb.Gettext
+    end
+  end
+
+  def join("game" <> _player, _payload, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+    case GameSupervisor.start_game(player) do
+    {:ok, _pid} ->
+    {:reply, :ok, socket}
+    {:error, reason} ->
+    {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
+  end
+
+end
